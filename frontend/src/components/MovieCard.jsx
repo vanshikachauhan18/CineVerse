e7b0cb898219579263de460f1ed899b35aa70eed
vanshikachauhan18 <vanshikachauhan1811@gmail.com>
2026-07-22 12:30:41 +0530

    Added favorite button to movie cards

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,41 +1,65 @@
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+import { addToFavorites, removeFromFavorites } from "../services/userService";
+
 import "./MovieCard.css";
 
 function MovieCard({ movie }) {
+  const [favorite, setFavorite] =useState(false);
+
+  const handleFavorite = async () => {
+    try {
+      if (favorite) {
+        await removeFromFavorites(movie._id);
+        toast.success("Removed from Favorites");
+      } else {
+        await addToFavorites(movie._id);
+        toast.success("Added to Favorites");
+      }
+
+      setFavorite(!favorite);
+    } catch (err) {
+      toast.error("Login to use Favorites");
+    }
+  };
+
   return (
-    <motion.div
-      className="movie-card"
-      whileHover={{ y: -10, scale: 1.03 }}
-      transition={{ duration: 0.3 }}
-    >
-      <div className="poster-container">
-        <img
-          src={movie.poster}
-          alt={movie.title}
-          className="movie-poster"
-          onError={(e) => {
-            e.target.src =
-              "https://placehold.co/300x450?text=No+Poster";
-          }}
-        />
+    <div className="movie-card">
+
+      <div className="favorite-btn" onClick={handleFavorite}>
+        {favorite ? <FaHeart /> : <FaRegHeart />}
       </div>
 
+      <img
+        src={movie.poster}
+        alt={movie.title}
+        className="movie-poster"
+      />
+
       <div className="movie-content">
+
         <h2>{movie.title}</h2>
 
-        <div className="movie-tags">
-          <span>🎭 {movie.genre}</span>
-          <span>📅 {movie.releaseYear}</span>
-        </div>
+        <p>
+          🎭 {movie.genre}
+        </p>
+
+        <p>
+          📅 {movie.releaseYear}
+        </p>
 
         <Link to={`/movie/${movie._id}`}>
           <button className="details-btn">
             View Details →
           </button>
         </Link>
+
       </div>
-    </motion.div>
+
+    </div>
   );
 }
 
